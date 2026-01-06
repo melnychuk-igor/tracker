ef966c3a45234804bf17ae8f5ec72c8d4c72337e
Melnychuk Igor <igor.melnychuck@gmail.com>
2026-01-06 15:22:59 +0200

    remove unnecessary classes

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,9 +5,7 @@ import TheContactLink from './TheContactLink.vue'
 </script>
 
 <template>
-  <header
-    class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3 md:max-xl:w-3 lg:w-3"
-  >
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
     <div class="flex items-center gap-4">
       <TheLogo />
       <TheContactLink />
